4b55cdc3a84f69a18cbb70c066ae107adc70fe63
Ashu Kumar <95637428+Ashu-kumar40@users.noreply.github.com>
2023-04-22 14:40:16 +0000

    useEffect missing depandency error fixed

--- src/Components/MovieHorizontalList/HorizontalList.js
+++ src/Components/MovieHorizontalList/HorizontalList.js
@@ -1,33 +1,27 @@
 import { React, useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import './HorizontalList.css'
+import "./HorizontalList.css";
 import Card from "../MovieCard/Card";
 
-export default function HorizontalList({movie_type}) {
-
+export default function HorizontalList({ movie_type }) {
   const [movieList, setMovieList] = useState([]);
   const [movieType, setMovieType] = useState(movie_type);
-  const {type} = useParams()
-  const ApiKey = "c546b3a17abcfc1c6c61807d1a0d0cf9";
+  const ApiKey = process.env.REACT_APP_API_KEY;
 
-  useEffect(() => (
-    getData()
-  ), [])
-
-  useEffect(() => (
-    getData()
-  ), [movieType])
-
-
-
-  // fetching the movies list
-  const getData = () => {
-    setMovieType(movie_type);
-    fetch(`https://api.themoviedb.org/3/movie/${movieType ? movieType : "popular"}?api_key=${ApiKey}&language=en-US&page=1`)
-      .then((res) => res.json())
-      .then((data) => setMovieList(data.results));
-  }
+  useEffect(() => {
+    // fetching the movies list
+    const getData = () => {
+      setMovieType(movie_type);
+      fetch(
+        `https://api.themoviedb.org/3/movie/${
+          movieType ? movieType : "popular"
+        }?api_key=${ApiKey}&language=en-US&page=1`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovieList(data.results));
+    };
+    getData();
+  }, [movie_type, movieType, ApiKey]);
 
   // adding functionality to the slide btn
 
@@ -36,15 +30,24 @@ export default function HorizontalList({movie_type}) {
       {/* Rendering Latest movies */}
       <div className="list-type-container">
         <div className="list-type">
-          <div className='heading-see-all-box'>
-            <h1 className="list-type-heading">{(movieType ? movieType : "LATEST").toUpperCase()}
+          <div className="heading-see-all-box">
+            <h1 className="list-type-heading">
+              {(movieType ? movieType : "LATEST").toUpperCase()}
             </h1>
-            <Link className="see-all" to={`movie/${movieType}`}>See All</Link>
+            <Link className="see-all" to={`movie/${movieType}`}>
+              See All
+            </Link>
           </div>
-          
+
           <div id="slideLeft" className="now-Playing">
             <button className="slide-btn">
-              <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>
+              <svg
+                focusable="false"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+              </svg>
             </button>
 
             {movieList.map((movie) => (
@@ -52,12 +55,17 @@ export default function HorizontalList({movie_type}) {
             ))}
 
             <button id="slideRight" className="slide-btn">
-              <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>
+              <svg
+                focusable="false"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+              </svg>
             </button>
           </div>
         </div>
       </div>
-      
     </>
   );
 }
